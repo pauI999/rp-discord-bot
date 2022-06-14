29a599aa48821f84a4ac44fe1bdf23b82dda1893
uneinqespielt <paulbaumi07@gmail.com>
2022-06-14 21:50:20 +0200

    added new feature

---
+++ context/route.js
@@ -0,0 +1,69 @@
+const config = require("../config.json");
+const Discord = require("discord.js");
+const functions = require("../functions/functions");
+
+module.exports = {
+  data: { name: "route", type: 2 },
+  async execute(interaction) {
+    if (config.routechannel !== "0") {
+      if (
+        functions.isLeaderschaft(interaction.member) ||
+        functions.isFamilienrat(interaction.member)
+      ) {
+        interaction.reply({
+          content: `Du hast jetzt 10 Sekunden Zeit, um in <#${config.routechannel}> die Anzahl reinzusenden! Wenn du nichts schreibst, passiert auch nichts`,
+          ephemeral: true,
+        });
+        let routechannel = interaction.channel.guild.channels.cache.get(
+          config.routechannel
+        );
+
+        const filter = (m) => {
+          return m.author.id === interaction.user.id;
+        };
+        const collector = routechannel.createMessageCollector({
+          filter,
+          max: 1,
+          time: 1000 * 10,
+        });
+        collector.on("collect", (message) => {
+          if (functions.isNumeric(message.content)) {
+            if (config.preisg !== "0") {
+              routechannel
+                .send(
+                  `<@${interaction.targetId}> hat ${functions.addDots(
+                    message.content
+                  )} ${config.droge} abgegeben → ${functions.addDots(
+                    message.content * config.preisavv
+                  )}$`
+                )
+                .then(() => {
+                  message.delete().catch((error) => {});
+                });
+            } else {
+              routechannel
+                .send(
+                  `<@${interaction.targetId}> hat ${functions.addDots(
+                    message.content
+                  )} ${config.droge} abgegeben`
+                )
+                .then(() => {
+                  message.delete().catch((error) => {});
+                });
+            }
+          }
+        });
+      } else {
+        interaction.reply({
+          content: "Fehler: Du hast nicht genug Rechte!",
+          ephemeral: true,
+        });
+      }
+    } else {
+      interaction.reply({
+        content: "Das Routenfeature ist nicht aktiviert!",
+        ephemeral: true,
+      });
+    }
+  },
+};
