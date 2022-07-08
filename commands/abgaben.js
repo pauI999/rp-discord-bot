83707b7be3431112e9c94af129c1c600776e3f72
uneinqespielt <paulbaumi07@gmail.com>
2022-07-08 03:00:23 +0200

    fix

--- commands/abgaben.js
+++ commands/abgaben.js
@@ -2,135 +2,6 @@ const { SlashCommandBuilder } = require("@discordjs/builders");
 const config = require("../config.json");
 const functions = require("../functions/functions");
 
-// Abgabenstatus ändern Funktion Interaction
-function toggleAbgaben(interaction, user, kw) {
-  let channel = interaction.guild.channels.cache.get(config.abgabenchannel);
-  let kassechannel;
-  if (config.kassechannel !== "0") {
-    kassechannel = interaction.guild.channels.cache.get(config.kassechannel);
-  }
-  let done = false;
-  channel.messages.fetch().then((messages) => {
-    messages.each((smessage) => {
-      if (smessage.content.includes(`**${kw}**`)) {
-        if (smessage.content.includes(user)) {
-          done = true;
-          let teile = smessage.content.split("\n");
-          let teileneu = [];
-          teile.forEach((teil) => {
-            if (teil.includes(user)) {
-              if (teil.includes(":x:")) {
-                teil = ` - <@${user}> - :white_check_mark:`;
-                if (config.kassechannel !== "0") {
-                  if (config.abgabenstring.includes("$")) {
-                    kassechannel.messages.fetch({ limit: 1 }).then((ms) => {
-                      ms.forEach(async (m) => {
-                        if (m.partial) await m.fetch();
-                        let msplit = m.content.split(" ");
-                        let currentamount = msplit[msplit.length - 1];
-                        if (currentamount.includes("$")) {
-                          currentamount = currentamount
-                            .replaceAll(".", "")
-                            .replace("$", "");
-                          currentamount = parseInt(currentamount);
-                          kassechannel.send(
-                            `+ ${
-                              config.abgabenstring
-                            } Abgaben ${kw} - <@${user}> \n\n> Frakkasse: ${functions.addDots(
-                              currentamount +
-                                parseInt(
-                                  config.abgabenstring
-                                    .replaceAll(".", "")
-                                    .replace("$", "")
-                                )
-                            )}$`
-                          );
-                        } else {
-                          kassechannel.send(
-                            `+ ${config.abgabenstring} Abgaben ${kw} - <@${user}>`
-                          );
-                        }
-                      });
-                    });
-                  }
-                }
-                functions.logEmbed(
-                  interaction.member,
-                  `Abgaben ${kw} von <@${user}> entgegengenommen`,
-                  `+ ${config.abgabenstring}`
-                );
-              } else {
-                teil = ` - <@${user}> - :x:`;
-                if (config.kassechannel !== "0") {
-                  if (config.abgabenstring.includes("$")) {
-                    kassechannel.messages.fetch({ limit: 1 }).then((ms) => {
-                      ms.forEach(async (m) => {
-                        if (m.partial) await m.fetch();
-                        let msplit = m.content.split(" ");
-                        let currentamount = msplit[msplit.length - 1];
-                        if (currentamount.includes("$")) {
-                          currentamount = currentamount
-                            .replaceAll(".", "")
-                            .replace("$", "");
-                          currentamount = parseInt(currentamount);
-                          kassechannel.send(
-                            `- ${
-                              config.abgabenstring
-                            } Abgaben ${kw} - <@${user}> \n\n> Frakkasse: ${functions.addDots(
-                              currentamount -
-                                parseInt(
-                                  config.abgabenstring
-                                    .replaceAll(".", "")
-                                    .replace("$", "")
-                                )
-                            )}$`
-                          );
-                        } else {
-                          kassechannel.send(
-                            `- ${config.abgabenstring} Abgaben ${kw} - <@${user}>`
-                          );
-                        }
-                      });
-                    });
-                  }
-                }
-                logEmbed(
-                  interaction.member,
-                  `Abgaben ${kw} an <@${user}> zurückgegeben`,
-                  `- ${config.abgabenstring}`
-                );
-              }
-              teileneu.push(teil);
-            } else {
-              teileneu.push(teil);
-            }
-          });
-          smessage.edit(teileneu.join("\n"));
-        } else {
-          interaction.reply({
-            content:
-              "Fehler: Das Mitglied muss diese Woche noch keine Abgaben zahlen!",
-            ephemeral: true,
-          });
-          return;
-        }
-      }
-    });
-    if (!done) {
-      interaction.reply({
-        content:
-          "Fehler: Die Nachricht für diese Kalenderwoche fehlt noch, oder liegt zu weit in der Vergangenheit!",
-        ephemeral: true,
-      });
-    } else {
-      interaction.reply({
-        content: `Der Abgabenstatus von ${user} in der ${kw}. Woche wurde geändert!`,
-        ephemeral: true,
-      });
-    }
-  });
-}
-
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("abgaben")
@@ -157,6 +28,7 @@ module.exports = {
         functions.isLeaderschaft(interaction.member) ||
         functions.isFamilienrat(interaction.member)
       ) {
+        console.log(interaction);
         if (interaction.options.getNumber("kalenderwoche") !== null) {
           if (interaction.options.getNumber("kalenderwoche") == 0) {
             interaction.reply({
@@ -171,7 +43,6 @@ module.exports = {
             );
           }
         } else {
-          console.log(interaction);
           functions.toggleAbgaben2(
             interaction,
             interaction.options.getUser("familienmitglied").id,
